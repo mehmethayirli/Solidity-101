f96a5b011d93251421fa824ce341a74f75bd586b
Mehmet <98388377+mehmethayirli@users.noreply.github.com>
2023-03-19 15:24:01 +0300

    Update Değişken Tipleri.sol

--- Değişken Tipleri.sol
+++ Değişken Tipleri.sol
@@ -12,7 +12,7 @@ contract degiskenTipleri{
     bool isSent = false;
 
     address account = 0x5051f6cEE99B81bD2d7E6C4a42A6C35b39c30423;
-    //address public owner = msg.sender;
+    //address public owner = msg.sender; //kullanici adresi
     bytes32 public string1 = "hello byte"; //byte
     string public string2 = "hello string"; //string
     
