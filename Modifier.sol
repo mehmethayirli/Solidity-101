6ef68d871570ea617dc1966711bda4bff2e92238
Mehmet {zk} <98388377+mehmethayirli@users.noreply.github.com>
2024-03-26 21:22:10 +0300

    Add files via upload

---
+++ Modifier.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Ownable {
+    address public owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can call this function");
+        _;
+    }
+
+    function transferOwnership(address newOwner) public onlyOwner {
+        require(newOwner != address(0), "Invalid address");
+        emit OwnershipTransferred(owner, newOwner);
+        owner = newOwner;
+    }
+    
+}
+
+contract MyContract is Ownable {
+    uint public myVariable;
+
+    function setMyVariable(uint newValue) public onlyOwner {
+        myVariable = newValue;
+    }
+}
